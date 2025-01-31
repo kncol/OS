ab0d300c263a36fb67a5ad3ebc092668e516c1c7
knol <knchrist1@icloud.com>
2025-01-31 14:03:29 -0500

    renamed

---
+++ stuff_done_in_class/1_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    int height,width;
+    printf("Type height of rectangle\n");
+    scanf("%d", &height);
+    printf("Type width of rectangle\n");
+    scanf("%d", &width);
+    printf("The area of the rectangl is: %d\n", height * width);
+}
--- stuff_done_in_class/evenNums.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int i;
-    for (i = 0; i <= 10; i+=2)
-    {
-        printf("%d\n",i);
-    }
-}
